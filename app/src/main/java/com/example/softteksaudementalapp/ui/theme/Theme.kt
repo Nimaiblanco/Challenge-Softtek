9df8d7cc46b05ca69d6c5a6cbf42244508fa2ffe
Nimai Blanco <133171939+Nimaiblanco@users.noreply.github.com>
2025-12-04 21:52:35 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/softteksaudementalapp/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package com.example.softteksaudementalapp.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+)
+
+private val DarkColors = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+@Composable
+fun SoftteksaudementalappTheme(
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
